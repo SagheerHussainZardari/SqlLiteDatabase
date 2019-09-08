3e1f54fd7c511c018e3200dff06b329a36f0ad29
MrZardari <sagheerhzardari@gmail.com>
2019-09-08 14:31:48 +0500

    Proper Way To CREATE DATABASE  , TABLE and INSERT data to that TABLE

--- app/src/main/java/com/example/sqllitedatabase/DbHelper.kt
+++ app/src/main/java/com/example/sqllitedatabase/DbHelper.kt
@@ -4,27 +4,43 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
-import android.widget.Toast
 
-class DbHelper(context: Context): SQLiteOpenHelper(context ,"Uni.db" , null , 1) {
+class DbHelper(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private val DATABASE_NAME = "University.db"
+        private val DATABASE_VERSION = 1
+
+        //table student
+        private val TABLE_NAME = "students"
+        private val STUDENTS_COL_NAME = "Name"
+        private val STUDENTS_COL_ROLL = "Roll"
+
+
+    }
+
     override fun onCreate(p0: SQLiteDatabase?) {
-        p0!!.execSQL("CREATE TABLE students(name TEXT , roll TEXT);")
+
+        val CREATE_TABLE_QUERY =
+            "CREATE TABLE $TABLE_NAME($STUDENTS_COL_NAME TEXT , $STUDENTS_COL_ROLL TEXT)"
+        p0!!.execSQL(CREATE_TABLE_QUERY)
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        p0!!.execSQL("DROP TABLE IF EXISTS students;")
-        onCreate(p0!!)
+
+        val DROP_TABLE_QUERY = "DROP TABLE IF EXISTS $TABLE_NAME"
+        p0!!.execSQL(DROP_TABLE_QUERY)
+        onCreate(p0)
     }
 
 
     fun insertData(name: String , roll: String){
-        var ra =  this.writableDatabase
+        val ra = this.writableDatabase
 
-        ra.execSQL("INSERT INTO students VALUES ( '$name', '$roll' )")
+        ra.execSQL("INSERT INTO $TABLE_NAME VALUES ( '$name', '$roll' )")
         Log.d("a" , "sagheer:row Inserted!!!")
 
     }
-
-
 
 }
